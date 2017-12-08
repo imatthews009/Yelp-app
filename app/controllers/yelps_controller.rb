1651d92c224ea2de847ff94cfae1708d09862985
Ian <imatthews009@gmail.com>
2017-12-08 10:11:19 -0800

    added figaro

--- app/controllers/yelps_controller.rb
+++ app/controllers/yelps_controller.rb
@@ -1,7 +1,7 @@
 class YelpsController < ApplicationController
     def index
       @response = Unirest.get "https://api.yelp.com/v3/businesses/search", 
-                  headers:{ Authorization: "Bearer "},
+                  headers:{ Authorization: "Bearer #{ENV["yelp_api_key"]}"},
                   parameters:{location: "94115", term: "food"}
 
     end
