1fdc286b3a4a660cdd870dc495dd31d707a6f2e7
Ian <imatthews009@gmail.com>
2017-12-07 18:18:29 -0800

    added yelp unirest request

--- app/controllers/yelps_controller.rb
+++ app/controllers/yelps_controller.rb
@@ -1,2 +1,8 @@
 class YelpsController < ApplicationController
+    def index
+      @response = Unirest.get "https://api.yelp.com/v3/businesses/search", 
+                  headers:{ Authorization: "Bearer pCluZ1j-zJynvHYngNa04hSZrClmgfe7oTUlMtMw9daYKRvOmkvvPO2PZbQpS8zNYCeuxnSa3HI9XfPVFAKxIkeEjll8TM7XMmY37JchCg3UY7QXVdhXNcfZINIpWnYx"},
+                  parameters:{location: "94115", term: "food"}
+
+    end
 end
