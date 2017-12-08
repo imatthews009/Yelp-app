6a2593f4286df1122250f831f37659d64de77d09
Ian <imatthews009@gmail.com>
2017-12-08 12:31:57 -0800

    added form and location field

--- app/controllers/yelps_controller.rb
+++ app/controllers/yelps_controller.rb
@@ -2,7 +2,7 @@ class YelpsController < ApplicationController
     def index
       @response = Unirest.get "https://api.yelp.com/v3/businesses/search", 
                   headers:{ Authorization: "Bearer #{ENV["yelp_api_key"]}"},
-                  parameters:{location: "94115", term: "food"}
+                  parameters:{location: params[:location], term: "food", limit: 10}
 
     end
 end
