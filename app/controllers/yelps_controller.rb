392d404a263d506e3c2a8f4279c45e6d8e62c1b4
Ian <imatthews009@gmail.com>
2017-12-08 16:09:39 -0800

    deleted index response

--- app/controllers/yelps_controller.rb
+++ app/controllers/yelps_controller.rb
@@ -1,8 +1,6 @@
 class YelpsController < ApplicationController
     def index
-      @response = Unirest.get "https://api.yelp.com/v3/businesses/search", 
-                  headers:{ Authorization: "Bearer #{ENV["yelp_api_key"]}"},
-                  parameters:{location: params[:location], term: "food", limit: 10, radius: params[:radius], categories: params[:categories]}
+
     end
 
     def show
