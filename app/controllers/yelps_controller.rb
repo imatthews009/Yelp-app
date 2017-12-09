1c07ce6f577e9ba270dfe1a1a5faf71783646c64
Ian <imatthews009@gmail.com>
2017-12-08 17:51:28 -0800

    added redirect if no match found

--- app/controllers/yelps_controller.rb
+++ app/controllers/yelps_controller.rb
@@ -7,7 +7,12 @@ class YelpsController < ApplicationController
         response = Unirest.get "https://api.yelp.com/v3/businesses/search",
                   headers:{ Authorization: "Bearer #{ENV["yelp_api_key"]}"},
                   parameters:{location: params[:location], term: "food", limit: 10, radius: params[:radius], categories: params[:categories]}
-        @restaurant = response.body["businesses"].sample(1)          
-        render "show.html.erb"
+        @restaurant = response.body["businesses"].sample(1) 
+        if @restaurant != []        
+          render "show.html.erb"
+        else
+          redirect_to "/yelps", :flash => { :error => "No matches found" }
+        end
+
     end
 end
